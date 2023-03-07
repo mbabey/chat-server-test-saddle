cb56c51d1799a19a0a4a72d6a36d2eda167cf9bc
Maxwell Babey <babeymax@gmail.com>
2023-03-07 11:07:54 -0800

    Implement validate ip function

--- src/saddle-setup.c
+++ src/saddle-setup.c
@@ -3,6 +3,8 @@
 
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 
 #define OPTS_LIST "l:i:p:t"
 #define USAGE_MESSAGE                                                                           \
@@ -37,6 +39,16 @@ static void trace_reporter(const char *file, const char *func, size_t line);
  */
 static int parse_args(struct state *state, int argc, char **argv);
 
+static int parse_lib(const char *lib_name, TRACER_FUNCTION_AS(tracer));
+
+static int parse_ip_and_port(struct sockaddr_in *addr, const char *port_num_str, const char *ip_addr_str,
+                             TRACER_FUNCTION_AS(tracer));
+
+static int validate_ip(struct sockaddr_in *addr, const char *ip_addr_str, TRACER_FUNCTION_AS(tracer));
+
+static int
+validate_port(in_port_t *port_num, const char *port_num_str, void (*tracer)(const char *, const char *, size_t));
+
 int setup_saddle(struct state *state, int argc, char **argv)
 {
     state->mm = init_mem_manager();
@@ -68,8 +80,9 @@ static void trace_reporter(const char *file, const char *func, size_t line)
 
 static int parse_args(struct state *state, int argc, char **argv)
 {
-    char c;
+    int c;
     
+    memset(state, 0, sizeof(*state));
     while ((c = getopt(argc, argv, OPTS_LIST)) != -1)
     {
         switch (c)
@@ -102,9 +115,73 @@ static int parse_args(struct state *state, int argc, char **argv)
         }
     }
     
-    if (check_state(state) == -1)
+    return 0;
+}
+
+static int parse_lib(const char *lib_type, TRACER_FUNCTION_AS(tracer))
+{
     
     return 0;
 }
 
-static
+static int parse_ip_and_port(struct sockaddr_in *addr, const char *port_num_str, const char *ip_addr_str,
+                             TRACER_FUNCTION_AS(tracer))
+{
+    PRINT_STACK_TRACE(tracer);
+    
+    int       port_err;
+    int       ip_err;
+    in_port_t port_num;
+    
+    memset(addr, 0, sizeof(struct sockaddr_in));
+    
+    port_err = validate_port(&port_num, port_num_str, tracer);
+    
+    ip_err = validate_ip(addr, ip_addr_str, tracer);
+    
+    if (port_err || ip_err)
+    {
+        return -1;
+    }
+    
+    addr->sin_port   = htons(port_num);
+    addr->sin_family = AF_INET;
+    
+    return 0;
+}
+
+static int validate_ip(struct sockaddr_in *addr, const char *ip_addr_str, TRACER_FUNCTION_AS(tracer))
+{
+    PRINT_STACK_TRACE(tracer);
+    
+    switch (inet_pton(AF_INET, ip_addr_str, &addr->sin_addr.s_addr))
+    {
+        case 1: // Valid
+        {
+            return 0;
+            break;
+        }
+        case 0: // Not a valid IP address
+        {
+            (void) fprintf(stderr, "%s is not a valid ip address\n", ip_addr_str);
+            return -1;
+            break;
+        }
+        default: // Some other error
+        {
+            return -1;
+            break;
+        }
+    }
+}
+
+static int
+validate_port(in_port_t *port_num, const char *port_num_str, void (*tracer)(const char *, const char *, size_t))
+{
+    PRINT_STACK_TRACE(tracer);
+    
+    *port_num = (in_port_t) strtol(port_num_str, NULL, 10);
+    
+    
+    return 0;
+}
