1c9bfee0e0ff10986856073f3fd689dc50b487ab
Maxwell Babey <babeymax@gmail.com>
2023-03-07 23:05:44 -0800

    Fix some problems with redefinition

--- saddle_functions.h
+++ saddle_functions.h
@@ -1,23 +1,21 @@
 #ifndef CHAT_TEST_SADDLE_SADDLE_FUNCTIONS_H
 #define CHAT_TEST_SADDLE_SADDLE_FUNCTIONS_H
 
-#include "./include/state.h"
-
 /**
  * Function cast type for library function.
  */
-#define LIB_FUNCTION (int (*)(struct state *state))
+#define LIB_FUNCTION (int (*)(void *))
 
 /**
  * Struct to store function pointers from library.
  */
 struct saddle_lib
 {
-    int (*lib_start)(struct state *state);
+    int (*lib_start)(void *);
     
-    int (*lib_run)(struct state *state);
+    int (*lib_run)(void *);
     
-    int (*lib_end)(struct state *state);
+    int (*lib_end)(void *);
 };
 
 /**
@@ -25,29 +23,29 @@ struct saddle_lib
  * <p>
  * Start function to be defined by library.
  * </p>
- * @param state the state object
+ * @param args the args object
  * @return 0 on success, -1 on failure
  */
-int lib_start(struct state *state);
+int lib_start(void *args);
 
 /**
  * lib_run
  * <p>
  * Run function to be defined by library.
  * </p>
- * @param state the state object
+ * @param args the args object
  * @return 0 on success, -1 on failure
  */
-int lib_run(struct state *state);
+int lib_run(void *args);
 
 /**
  * lib_end
  * <p>
  * End function to be defined by library.
  * </p>
- * @param state the state object
+ * @param args the args object
  * @return 0 on success, -1 on failure
  */
-int lib_end(struct state *state);
+int lib_end(void *args);
 
 #endif //CHAT_TEST_SADDLE_SADDLE_FUNCTIONS_H
