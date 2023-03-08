78a9678a83501755abb9868383455e3cf0be793c
Maxwell Babey <babeymax@gmail.com>
2023-03-08 09:39:31 -0800

    Add open socket

--- client-test-saddle-source/src/lib-main.c
+++ client-test-saddle-source/src/lib-main.c
@@ -1,14 +1,50 @@
 #include "../../include/saddle-functions.h"
 #include "../include/state.h"
 
+/**
+ * open_socket_connect
+ * <p>
+ * Create a socket and connect to an IP address on that socket.
+ * </p>
+ * @param state the state object
+ * @param client the client object
+ * @return 0 on success, -1 and set err on failure
+ */
+static int open_socket_connect(struct state_minor *state, struct client *client);
+
 int lib_main(void *args)
 {
     struct state_minor *state = (struct state_minor *) args;
     struct client client;
     
-    // Create a socket with the address passed in state
+    if (open_socket_connect(state, &client) == -1)
+    {
+        return -1;
+    }
     
-    // connect to the server
+    return 0;
+}
+
+static int open_socket_connect(struct state_minor *state, struct client *client)
+{
+    int fd;
+    socklen_t socklen;
+    
+    fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (fd == -1)
+    {
+        SET_ERROR(state->err);
+        return -1;
+    }
+    
+    socklen = sizeof(state->addr);
+    if (connect(fd, (struct sockaddr *) &state->addr, socklen) == -1)
+    {
+        SET_ERROR(state->err);
+        return -1;
+    }
+    
+    client->socket_fd = fd;
     
     return 0;
 }
--- include/error-handlers.h
+++ include/error-handlers.h
@@ -1,6 +1,7 @@
 #ifndef CHAT_TEST_SADDLE_ERROR_HANDLERS_H
 #define CHAT_TEST_SADDLE_ERROR_HANDLERS_H
 
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
