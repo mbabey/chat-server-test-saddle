3ae8339d89c928fafd0e920dcff2c26b51736db3
Maxwell Babey <babeymax@gmail.com>
2023-03-08 11:08:31 -0800

    Add ui.h/.c and declare UI functions

--- client-test-saddle-source/include/ui.h
+++ client-test-saddle-source/include/ui.h
@@ -1,4 +1,27 @@
 #ifndef CLIENT_TEST_SADDLE_UI_H
 #define CLIENT_TEST_SADDLE_UI_H
 
+#include "state.h"
+
+/**
+ * run_ui
+ * <p>
+ * Display the user interface menu on stdout. Get user input.
+ * </p>
+ * @param state the state object
+ * @param client tne client object
+ * @return 0 on success, -1 and set err on failure.
+ */
+int run_ui(struct state_minor *state, struct client *client);
+
+/**
+ * display_results
+ * <p>
+ * Display the results of a test. Wait for a key press before exiting the function.
+ * </p>
+ * @param state the state object
+ * @return 0 on success, -1 and set err on failure.
+ */
+int display_results(struct state_minor *state);
+
 #endif //CLIENT_TEST_SADDLE_UI_H
---
+++ client-test-saddle-source/src/ui.c
@@ -0,0 +1,21 @@
+#include "../include/ui.h"
+
+int run_ui(struct state_minor *state, struct client *client)
+{
+    // Clear the screen
+    
+    // Print a big ass menu
+    
+    // Take user input
+    
+    return 0;
+}
+
+int display_results(struct state_minor *state)
+{
+    // Display the results
+    
+    // Wait for key press
+    
+    return 0;
+}
