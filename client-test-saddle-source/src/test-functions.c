934c97b076df018266b806776d33543133114a1c
Maxwell Babey <babeymax@gmail.com>
2023-03-08 20:53:32 -0800

    Implement dummy test functions

---
+++ client-test-saddle-source/src/test-functions.c
@@ -0,0 +1,107 @@
+#include "../include/test-functions.h"
+
+#include <stdio.h>
+
+int create_user_test(void)
+{
+    printf("create_user_test\n");
+    
+    return 0;
+}
+
+int create_channel_test(void)
+{
+    printf("create_channel_test\n");
+    
+    return 0;
+}
+
+int create_message_test(void)
+{
+    printf("create_message_test\n");
+    
+    return 0;
+}
+
+int create_auth_test(void)
+{
+    printf("create_auth_test\n");
+    
+    return 0;
+}
+int read_user_test(void)
+{
+    printf("read_user_test\n");
+    
+    return 0;
+}
+
+int read_channel_test(void)
+{
+    printf("read_channel_test\n");
+    
+    return 0;
+}
+
+int read_message_test(void)
+{
+    printf("read_message_test\n");
+    
+    return 0;
+}
+
+int update_user_test(void)
+{
+    printf("update_user_test\n");
+    
+    return 0;
+}
+
+int update_channel_test(void)
+{
+    printf("update_channel_test\n");
+    
+    return 0;
+}
+
+int update_message_test(void)
+{
+    printf("update_message_test\n");
+    
+    return 0;
+}
+
+int update_auth_test(void)
+{
+    printf("update_auth_test\n");
+    
+    return 0;
+}
+
+int destroy_user_test(void)
+{
+    printf("destroy_user_test\n");
+    
+    return 0;
+}
+
+int destroy_channel_test(void)
+{
+    printf("destroy_channel_test\n");
+    
+    return 0;
+}
+
+int destroy_message_test(void)
+{
+    printf("destroy_message_test\n");
+    
+    return 0;
+}
+
+int destroy_auth_test(void)
+{
+    printf("destroy_auth_test\n");
+    
+    return 0;
+}
