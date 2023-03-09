7524be611febd8dcf3ff7a25a9e236116c53c388
Maxwell Babey <babeymax@gmail.com>
2023-03-08 22:49:59 -0800

    Add type and object enums

--- client-test-saddle-source/include/state.h
+++ client-test-saddle-source/include/state.h
@@ -6,14 +6,18 @@
 #include <mem_manager/manager.h>
 #include <netinet/in.h>
 
+
+
 /**
  * Minor state struct. See chat-server-test-saddle/include/state.h for major.
  */
 struct state_minor
 {
-    struct sockaddr_in addr;
-    struct error_saver err;
+    struct sockaddr_in    addr;
+    struct error_saver    err;
+    
     TRACER_FUNCTION_AS(tracer);
+    
     struct memory_manager *mm;
 };
 
@@ -29,12 +33,34 @@ struct client
 /**
  * A Network Dispatch.
  */
-struct dispatch {
-    uint8_t version : 4;
-    uint8_t type : 4;
-    uint8_t object;
+struct dispatch
+{
+    uint8_t  version: 4;
+    uint8_t  type: 4;
+    uint8_t  object;
     uint16_t body_size;
-    char *body;
+    char     *body;
+};
+
+/**
+ * Dispatch Types
+ */
+enum Type
+{
+    CREATE = 1,
+    READ,
+    UPDATE,
+    DESTROY,
+    PINGUSER = 9,
+    PINGCHANNEL
+};
+
+enum Object
+{
+    USER = 1,
+    CHANNEL,
+    MESSAGE,
+    AUTH
 };
 
 #endif //CLIENT_TEST_SADDLE_STATE_H
