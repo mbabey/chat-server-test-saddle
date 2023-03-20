8f0fa8319d32cfd7086765bea907ca64cd798e1a
Maxwell Babey <babeymax@gmail.com>
2023-03-19 17:55:01 -0700

    Define serialize user function

--- server-test-saddle-source/src/db.c
+++ server-test-saddle-source/src/db.c
@@ -2,6 +2,18 @@
 #include "../include/db.h"
 
 /**
+ * serialize_user
+ * <p>
+ * Serialize a User struct.
+ * </p>
+ * @param co the core object
+ * @param serial_user the buffer into which to serialize the user
+ * @param user the user to serialize
+ * @return 0 on success, -1 and set err on failure
+ */
+static int serialize_user(struct core_object *co, uint8_t **serial_user, User *user);
+
+/**
  * print_db_error
  * <p>
  * Print an error message based on the error code of passed.
@@ -30,7 +42,6 @@ int db_create(struct core_object *co, struct server_object *so, int type, void *
             serial_user_size = serialize_user(&serial_user, user);
             if (serial_user_size == -1)
             {
-                SET_ERROR(co->err);
                 return -1;
             }
             
@@ -112,6 +123,28 @@ int db_create(struct core_object *co, struct server_object *so, int type, void *
     return 0;
 }
 
+static int serialize_user(struct core_object *co, uint8_t **serial_user, User *user)
+{
+    PRINT_STACK_TRACE(co->tracer);
+    
+    size_t byte_offset;
+    
+    *serial_user = mm_malloc(sizeof(user->id) + strlen(user->display_name) + 1 + sizeof(user->privilege_level), co->mm);
+    if (!*serial_user)
+    {
+        SET_ERROR(co->err);
+        return -1;
+    }
+    
+    memcpy(*serial_user, &user->id, sizeof(user->id));
+    byte_offset = sizeof(user->id);
+    memcpy(*(serial_user + byte_offset), user->display_name, strlen(user->display_name) + 1);
+    byte_offset += strlen(user->display_name) + 1;
+    memcpy(*(serial_user + byte_offset), &user->privilege_level, sizeof(user->privilege_level));
+    
+    return 0;
+}
+
 int db_read(struct core_object *co, struct server_object *so, int type, void *object)
 {
     PRINT_STACK_TRACE(co->tracer);
@@ -154,7 +187,8 @@ static void print_db_error(int err_code)
         }
         case 4:
         {
-            (void) fprintf(stdout, "Database error occurred: An I/O error occurred while reading or writing the database file.\n");
+            (void) fprintf(stdout,
+                           "Database error occurred: An I/O error occurred while reading or writing the database file.\n");
             break;
         }
         case 5:
@@ -169,7 +203,8 @@ static void print_db_error(int err_code)
         }
         case 7:
         {
-            (void) fprintf(stdout, "Database error occurred: The specified key or value was too long to be stored in the database.\n");
+            (void) fprintf(stdout,
+                           "Database error occurred: The specified key or value was too long to be stored in the database.\n");
             break;
         }
         case 8:
